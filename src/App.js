e3e62cb2e83ff3d4eee0e6b69260a01521adcb46
ZuhebAkhter <zuheb1998@gmail.com>
2023-08-17 14:07:24 +0530

    authenticstion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Counter from './components/Counter';
-
+import React from 'react';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile'
 
 function App() {
+  const Authn=useSelector(state => state.auth.isAuthenticated)
+
   return (
+    <React.Fragment>\
+      <Header/>
+      {!Authn && <Auth/>}
+      {Authn && <UserProfile/>}
     <Counter />
+    </React.Fragment>
   );
 }
 
